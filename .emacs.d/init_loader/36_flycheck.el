bda107a16c99f2a40d1e50fd391a4d998d716884
vios-fish <vios.fish@gmail.com>
2014-04-25 01:47:21 +0900

    update flycheck init. add flycheck-pos-tip to setup.el

--- .emacs.d/init_loader/36_flycheck.el
+++ .emacs.d/init_loader/36_flycheck.el
@@ -1,14 +1,24 @@
 ;;; flycheck
 (require 'flycheck)
-(flycheck-define-checker c/c++
-  "A C/C++ checker using g++."
-  :command ("/usr/bin/g++" "-Wall" "-Wextra" "-std=c++11" source)
-  :error-patterns  ((error line-start
-                           (file-name) ":" line ":" column ":" " エラー: " (message)
-                           line-end)
-                    (warning line-start
-                           (file-name) ":" line ":" column ":" " 警告: " (message)
-                           line-end))
+(flycheck-define-checker c/c++-clang
+  "A C/C++ checker using clang."
+  :command ("/usr/bin/clang" "-Wall" "-Wextra" "-std=c++11" source)
+  :error-patterns
+  ((error line-start
+          (message "In file included from") " " (file-name) ":" line ":"
+          line-end)
+   (info line-start (file-name) ":" line ":" column
+		 ": note: " (message) line-end)
+   (warning line-start (file-name) ":" line ":" column
+            ": warning: " (message) line-end)
+   (error line-start (file-name) ":" line ":" column
+          ": " (or "fatal error" "error") ": " (message) line-end))
+
   :modes (c-mode c++-mode))
+
+;;; flycheck error view by tool tip
+(eval-after-load 'flycheck
+  '(custom-set-variables
+   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
