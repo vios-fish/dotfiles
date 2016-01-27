7564f5bbff6112baf3896146244590a78694a5df
tokunaga <tokunaga@human.ait.kyushu-u.ac.jp>
2016-01-27 16:56:06 +0900

    modified flycheck

--- .emacs.d/init_loader/36_flycheck.el
+++ .emacs.d/init_loader/36_flycheck.el
@@ -1,26 +1,33 @@
-;;; flycheck
-(require 'flycheck)
+;;; package --- Summary
+;;; Commentary:
 
+
+;;; Code:
+
+;;; flycheck clang include path
 (setq flycheck-clang-include-path '("/usr/local/Cellar/glpk/4.52/include" "/Users/tokunagamakoto/Programing/uchidalab/HigherMarkovTracking/src/"))
 
-(flycheck-define-checker c/c++-clang
-  "A C/C++ checker using clang."
-  :command ("clang" "-Wall" "-Wextra" "-std=c++11"
-            (option-list "-I" flycheck-clang-include-path)
-			source)
-  :error-patterns
-  ((error line-start
-          (message "In file included from") " " (file-name) ":" line ":"
-          line-end)
-   (info line-start (file-name) ":" line ":" column
-		 ": note: " (message) line-end)
-   (warning line-start (file-name) ":" line ":" column
-            ": warning: " (message) line-end)
-   (error line-start (file-name) ":" line ":" column
-          ": " (or "fatal error" "error") ": " (message) line-end))
+;;; flycheck for c++11
+;; (flycheck-define-checker c/c++-clang
+;;   "A C/C++ checker using clang."
+;;   :command ("clang" "-Wall" "-Wextra" "-std=c++11"
+;;             (option-list "-I" flycheck-clang-include-path)
+;; 			source)
+;;   :error-patterns
+;;   ((error line-start
+;;           (message "In file included from") " " (file-name) ":" line ":"
+;;           line-end)
+;;    (info line-start (file-name) ":" line ":" column
+;; 		 ": note: " (message) line-end)
+;;    (warning line-start (file-name) ":" line ":" column
+;;             ": warning: " (message) line-end)
+;;    (error line-start (file-name) ":" line ":" column
+;;           ": " (or "fatal error" "error") ": " (message) line-end))
 
-  :modes (c-mode c++-mode))
+;;   :modes (c-mode c++-mode))
 
+;; 上記設定のかわり
+(add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++11")))
 
 ;;; flycheck error view by tool tip
 (eval-after-load 'flycheck
