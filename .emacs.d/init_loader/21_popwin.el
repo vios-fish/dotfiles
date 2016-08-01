047a8f9aad999b0eed0dda9a74a618e46a7d7aa3
vios-fish <tokunaga@human.ait.kyushu-u.ac.jp>
2016-08-01 22:43:34 +0900

    fix emacs config for helm

--- .emacs.d/init_loader/21_popwin.el
+++ .emacs.d/init_loader/21_popwin.el
@@ -1,13 +1,16 @@
-;;;
-;;; popwin
-;;; バッファをホップアップウィンドウで表示
-(require 'popwin)
-(setq display-buffer-function 'popwin:display-buffer)
-;; anythingバッファをボップアップ表示
-(setq anything-samewindow nil)
-(push '("*helm*" :height 20) popwin:special-display-config)
-(push '(" *my-anything-filelist+*" :height 30) popwin:special-display-config)
-(push '("*undo-tree*" :height 30) popwin:special-display-config)
-;; diredバッファをボップアップ表示
-(push '(dired-mode :position top) popwin:special-display-config)
+;;; package -- summery
+;;; Commentary:
 
+;;; Code:
+
+(use-package popwin
+  :config
+  (progn
+	(popwin-mode 1)
+	(setq display-buffer-function 'popwin:display-buffer)
+	;; diredバッファをボップアップ表示
+	(push '(dired-mode :position top) popwin:special-display-config)))
+  
+
+(provide '21_popwin)
+;;; 21_popwin.el ends here
--- .emacs.d/init_loader/34_undo-tree.el
+++ .emacs.d/init_loader/34_undo-tree.el
@@ -1,8 +1,11 @@
 ;;; undo-tree
 ;;; undo,redoを木状にしてやりやすくする
 
-(use-package undo-tree
-  :diminish "UT"
+(use-package undo-tree :diminish "UT"
+  :after popwin
   :init
-  (global-undo-tree-mode))
+  (global-undo-tree-mode)
+  
+  :config
+  (push '("*undo-tree*" :height 30) popwin:special-display-config))
 
